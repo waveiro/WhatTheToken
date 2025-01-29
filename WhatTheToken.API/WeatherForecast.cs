4e0d20f3077077f148afc94b5b331114d3c77253
Willian Arsenio de Sousa <willian@canva.com>
2025-01-30 09:08:34 +1100

    bump version of model

--- WhatTheToken.API/WeatherForecast.cs
+++ WhatTheToken.API/WeatherForecast.cs
@@ -6,6 +6,6 @@ public class WeatherForecast
     public DateOnly Date { get; set; }
     public int TemperatureC { get; set; }
     public string? Summary { get; set; }
-    public string Version => "2.0";
+    public string Version => "2.1";
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
