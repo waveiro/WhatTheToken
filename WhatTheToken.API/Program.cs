e67dd60d425a7419356f81f627ce840c5c3712be
Willian Arsenio de Sousa <willian@canva.com>
2025-02-02 21:05:27 +1100

    add home

--- WhatTheToken.API/Program.cs
+++ WhatTheToken.API/Program.cs
@@ -9,16 +9,8 @@ builder.Services.AddDbContext<WeatherDbContext>(options => options.UseNpgsql(con
 // Add services to the container.
 builder.Services.AddControllers();
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
